5af73231a0a62b09dfd9b6eab349f11af4d1e6bb
Gian <gjs88@cornell.edu>
2020-10-16 11:17:07 -0400

    addition of pretty printing and open statement to testing file

---
+++ test.ml
@@ -0,0 +1,22 @@
+open OUnit2
+open Player
+open Command
+open Game
+open Course
+
+
+(** [pp_string s] pretty-prints string [s]. *)
+let pp_string s = "\"" ^ s ^ "\""
+
+(** [pp_list pp_elt lst] pretty-prints list [lst], using [pp_elt]
+    to pretty-print each element of [lst]. *)
+let pp_list pp_elt lst =
+  let pp_elts lst =
+    let rec loop n acc = function
+      | [] -> acc
+      | [h] -> acc ^ pp_elt h
+      | h1 :: (h2 :: t as t') ->
+        if n = 100 then acc ^ "..."  (* stop printing long list *)
+        else loop (n + 1) (acc ^ (pp_elt h1) ^ "; ") t'
+    in loop 0 "" lst
+  in "[" ^ pp_elts lst ^ "]"
