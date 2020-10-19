0860e0159862bbffc90092b271c990a0e7c4da12
Jenna <jjr265@cornell.edu>
2020-10-19 15:51:38 -0400

    idk

--- test.ml
+++ test.ml
@@ -67,8 +67,6 @@ let game_tests =
   [
   ]
 
-
-
 let player_name_test (name : string) (input : Player.t) (exp_output : string) : 
   test = name >:: (fun _ -> 
     assert_equal exp_output (Player.get_player_name input) ~printer:pp_string)
