5066f597e03164d46875323b806a5be0386c46f5
Gian <gjs88@cornell.edu>
2020-10-20 08:38:57 -0400

    Updated abstraction functions and specifications

--- player.ml
+++ player.ml
@@ -3,8 +3,41 @@ open Yojson.Basic.Util
 (* Implementations of player and it's functions*)
 (*****************************************************)
 
+(* 
+The type that represents the handicap of a player. The interpretation is the
+same as in normal golf. The value is subtracted from a player's final score at
+the end of a match. This allows players of different skill levels to play
+with each other.  *)
 type handicap = int
+
+(* 
+Abstraction Function: Some golf players are more or less accurate than others
+due to years of practice, a steady hand, or a good eye. This is The type that 
+represents an "accuracy multiplier". It is assumed a
+completely average player would have a multiplier of 1.0. A more accurate 
+player could have a mutiplier ranging from 1.0 up to 1.5 inclusive. A less
+accurate player could have a multiplier ranging from 1.0 down to 0.5 inclusive.
+In the game itself, the accuracy multiplier will allow the player to reduce
+(for a more accurate player) or increase (for a less accurate player) the 
+amount their ball deviates from where they wanted it to go (reduce the noise 
+from each swing due to random variation or wind)
+
+Representation Invariant: The accuracy_multiplier must be between 0.5 and 1.5 
+inclusive. *)
 type accuracy_multiplier = float
+
+(* 
+Abstraction Function: Some golf players are stronger or weaker than others. 
+This is The type that represents a "power multiplier". It is assumed a
+completely average player would have a multiplier of 1.0. A stronger player 
+could have a mutiplier ranging from 1.0 up to 1.5 inclusive. A weaker player
+could have a multiplier ranging from 1.0 down to 0.5 inclusive. In the game
+itself, this multiplier will allow the player to hit the ball farther (for a 
+stronger player) or less far (for a weaker player) given the same 
+representation of power. 
+
+Representation Invariant: The power_multiplier must be between 0.5 and 1.5 
+inclusive. *)
 type power_multiplier = float
 
 type t ={
--- player.mli
+++ player.mli
@@ -10,41 +10,15 @@
 (**The abstract type of values representing a player *)
 type t
 
-(* 
-Abstraction Function: Some golf players are stronger or weaker than others. 
-This is The type that represents a "power multiplier". It is assumed a
-completely average player would have a multiplier of 1.0. A stronger player 
-could have a mutiplier ranging from 1.0 up to 1.5 inclusive. A weaker player
-could have a multiplier ranging from 1.0 down to 0.5 inclusive. In the game
-itself, this multiplier will allow the player to hit the ball farther (for a 
-stronger player) or less far (for a weaker player) given the same 
-representation of power. 
 
-Representation Invariant: The power_multiplier must be between 0.5 and 1.5 
-inclusive. *)
-
+(*The type representing how much a player's strength effects their swing power*)
 type power_multiplier = float
-(* 
-Abstraction Function: Some golf players are more or less accurate than others
-due to years of practice, a steady hand, or a good eye. This is The type that 
-represents an "accuracy multiplier". It is assumed a
-completely average player would have a multiplier of 1.0. A more accurate 
-player could have a mutiplier ranging from 1.0 up to 1.5 inclusive. A less
-accurate player could have a multiplier ranging from 1.0 down to 0.5 inclusive.
-In the game itself, the accuracy multiplier will allow the player to reduce
-(for a more accurate player) or increase (for a less accurate player) the 
-amount their ball deviates from where they wanted it to go (reduce the noise 
-from each swing due to random variation or wind)
 
-Representation Invariant: The accuracy_multiplier must be between 0.5 and 1.5 
-inclusive. *)
+(*The type representing how much a player's skill effects their swing accuracy*)
 type accuracy_multiplier = float
 
-(* 
-The type that represents the handicap of a player. The interpretation is the
-same as in normal golf. The value is subtracted from a player's final score at
-the end of a match. This allows players of different skill levels to play
-with each other.  *)
+(*The type representing how many additional strokes a player gets subtracted to 
+  their score based on their skill level*)
 type handicap = int
 
 
