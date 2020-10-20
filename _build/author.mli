2f80e0e52f4b9560fd27cad51a41c1be03e73a5c
Jenna <jjr265@cornell.edu>
2020-10-20 16:50:43 -0400

    make clean scoring system so far

--- _build/author.mli
+++
@@ -1,8 +0,0 @@
-(** CS 3110 Fall 2020 Final Project
-    @author Rebekah Westerlind (rw537) 
-    @author Gian Soriano (gjs88)
-    @author Jennifer Russell (jjr265) *)
-
-
-(** [hours_worked] is the number of hours you worked on this assignment. *)
-val hours_worked : int
