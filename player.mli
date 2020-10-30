98bd5777e5fd6c1d7e4e3433abd4e8098ac22a66
Rebekah W <rw537@cornell.edu>
2020-10-30 12:06:39 -0400

    Implemented read_players for testing purposes

--- player.mli
+++ player.mli
@@ -27,6 +27,9 @@ type handicap = int
      Raises: Invalid_argument if input is not one of the accepted forms *)
 val init_players : unit -> t array
 
+(** [read_players j] produces an array of players based on input from a json *)
+val read_players : Yojson.Basic.t -> t array
+
 (** *)
 val get_player_name : t -> string
 
