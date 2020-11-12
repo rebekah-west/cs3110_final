ea7e52ade99defc22e2ce19fc07e2637afb283e3
Rebekah W <rw537@cornell.edu>
2020-11-12 14:37:33 -0500

    Added help menu descriptions

--- help.ml
+++ help.ml
@@ -13,7 +13,14 @@ let game_explanation =
   Each hole will be played according to the rules of golf. After completing all
   the holes, the game will end and a winner will be declared.\n"
 
-let swing_input = "yay i love describing\n"
+let swing_input = 
+  "When it becomes your turn, you are prompted to enter information about 
+  your swing. You will be asked 4 different questions to determine your swing.
+  The first is which club you would like to use. The second is how hard you 
+  want to hit the ball. The third is what angle up from the ground you want 
+  the ball to travel (think of the difference between a line drive and a 
+  rainbow). The fourth is alignment, such as hitting the ball to the left of 
+  the hole to account for wind, avoiding obstacles, slope of the green etc. \n"
 
 let who_goes_first = 
   "The first person to enter their information will go first on the first hole. 
@@ -26,40 +33,82 @@ let scoring_explanation =
   Wait idk how golf scoring works. Par...? Handicap...?\n"
 
 (* for player initialization menu*)
-let num_players_explanation = "TODO"
-let name_explanation = "TODO"
-let skill_explanation = "TODO"
-let strength_explanation = "TODO"
-let handicap_explanation = "TODO"
+let num_players_explanation = 
+  "The number of players represents how many people will be playing golf during 
+  this round. For example, if you are playing with two friends, you should 
+  enter the number 3. \n"
+let name_explanation = 
+  "The name entered will be used to represent you throughout the game. It will 
+  be referenced at the start and end of your turn as well as in the printed 
+  scores. \n"
+let skill_explanation = 
+  "Skill represents your estimated skill at golf. Since swings are calculated 
+  with math instead of physical laws of nature, we factor this in to improve 
+  your swings. However, just like real life, there is an element of randomness 
+  since every good golf player knows that skill is not a guarantee of 
+  perfection with every swing. \n"
+let strength_explanation = 
+  "Strength represents a baseline power behind each swing. Stronger players 
+  will hit the ball further with less effort, while weaker players will have 
+  to swing harder to have the same result. \n"
+let handicap_explanation = 
+  "The handicap represents a feature from golf that allows players of varying 
+  skill levels to be on more equal playing ground. For those who are 
+  significantly worse at golf, they can enter a handicap which will be 
+  subtracted from their final score. \n"
 
 (*for clubs menu*)
 let driver_explanation = "The driver is a club that will increase your power
 by 50% but will result in a decrease in a 20% decrease inyour accuracy. It is 
-best used for very long shots"
+best used for very long shots. \n"
 let nine_iron_explanation = "The Nine Iron will decrease your power by 10% but
-increase your accuracy by 10%. It is best for mid-long range shots."
+increase your accuracy by 10%. It is best for mid-long range shots.\n"
 let eight_iron_explanation = "The Eight Iron will slightly decrease your power 
 by 20% but increase your accuracy by 20%. It is best used for mid-long 
-range shots."
+range shots.\n"
 let putter_explanation = "The putter will decrease your power by 50% but
- increase your accuracy by 30%. It is best for shots very close to the hole."
+ increase your accuracy by 30%. It is best for shots very close to the hole.\n"
 let sand_wedge_explanation = "The sand wedge will decrease your power by 20%
-but increase your power by 10%. It is best for mid-ranged shots."
+but increase your power by 10%. It is best for mid-ranged shots.\n"
 let pitching_wedge_explanation = "The pitching wedge will decrease your power 
 by 30% but increase your accuracy by 50%. It is best used for short to 
-mid-ranged shots."
+mid-ranged shots.\n"
 
 (* for player input help menu*)
-let power_explanation = "TODO"
-let angle_explanation = "TODO"
-let alignment_explanation = "TODO"
+let power_explanation = 
+  "Power impacts how far the golf ball will travel on that swing. \n"
+let angle_explanation = 
+  "Angle impacts how arced the path travelled by the golf ball will be. \n"
+let alignment_explanation = 
+  "Alignment impacts which direction the golf ball will travel on that swing. 
+  A negative number represents aiming to the left of the hole, 0 represents 
+  aiming directly at the hole, and a positive number represents aiming to the 
+  right of the hole. \n"
 
 (* for scoring help menu *)
-let turn_explanation = "TODO"
-let par_explanation = "TODO"
-let stroke_limit_explanation = "TODO"
-let winning_a_hole_explanation = "TODO"
-let winning_the_game_explanation = "TODO"
+let turn_explanation = 
+  "On each turn, a player will be shown the layout of their course, including 
+  their location, the location of the hole, and the locations of any obstacles. 
+  They will then be prompted to enter input for their swing (club, power, 
+  angle, alignment). The trajectory of the golf ball will be calculated and 
+  their new location will be shown. The play will then transition to the next 
+  player's turn. \n"
+let par_explanation = 
+  "Par is a traditional golf concept that we have implemented in this game. 
+  By way of definition, par is the predetermined number of strokes that a 
+  proficient golfer should require to complete a hole. \n"
+let stroke_limit_explanation = 
+  "To avoid excessively long games, after 10 strokes a player's attempt to get 
+  the ball in the hole will be truncated and they will be given their current 
+  score on that hole. \n"
+let winning_a_hole_explanation = 
+  "The player who gets their ball in the hole in the fewest number of strokes 
+  is considered the winner for that hole. They will then be the player who 
+  gets to go first on the next hole. \n"
+let winning_the_game_explanation = 
+  "Score is aggregated over all of the holes players. When all holes have 
+  been played to completion, the winning player is the player with the lowest 
+  overall score. \n"
 
 let scoring_menu_parser selection =
   match selection with 
