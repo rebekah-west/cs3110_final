8322b04e409567aaf22c502a6a4e1fe39defc815
Jenna <jjr265@cornell.edu>
2020-12-08 16:42:38 -0500

    merge fist

--- main.ml
+++ main.ml
@@ -23,21 +23,23 @@ let pp_list pp_elt lst =
 let play_game f =
   let course = Course.from_json(Yojson.Basic.from_file(f)) in
   let players = Player.init_players () in
-  let game = Game.init_game players course in
-  scorecard_printer game course;
-  let rec hole game hole_num = 
-    let valid = hole_num <= (Course.num_holes course) in
-    match valid with
-    | false -> Printf.printf "Congratulations! You have completed the course.";
-    | true -> hole (Game.play_hole game) (hole_num+1);
-      if valid then scorecard_printer game course;
-  in hole game 0;
+  let game = ref (Game.init_game players course) in
+  scorecard_printer !game course;
+  (* let rec hole game hole_num = 
+     let valid = (hole_num ) <= (Course.num_holes course) in
+     match valid with
+     | false -> Printf.printf "Congratulations! You have completed the course.";
+     | true -> hole (Game.play_hole game) (hole_num+1)
+     in hole game 0; *)
+  for i=0 to Array.length (Course.get_holes course)-1 do 
+    game := (Game.play_hole !game)
+  done;
   let winners = Array.to_list 
-      (Array.map Player.get_player_name (Game.winner_of_game game)) in
+      (Array.map Player.get_player_name (Game.winner_of_game !game)) in
   Printf.printf "The winner is %s" (pp_list pp_string winners);
   print_string "\n";
   print_string "The complete scorecard is: " ;
-  scorecard_printer game course;
+  scorecard_printer !game course;
   Printf.printf "Thank you for visiting Golf, Inc. We hope you come back soon."
 
 
